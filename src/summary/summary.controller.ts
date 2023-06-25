c245ce5e1c5ed430263803979002e8974a148f56
Pikipici <pikipici@protonmail.com>
2023-06-25 22:40:48 +0700

    commit terakhir

--- src/summary/summary.controller.ts
+++ src/summary/summary.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable prettier/prettier */
+import { Controller, Get } from '@nestjs/common';
+import { SummaryService } from './summary.service';
 
 @Controller('summary')
-export class SummaryController {}
+export class SummaryController {
+  constructor(private readonly summaryService: SummaryService) {}
+
+  @Get()
+  getSummary() {
+    return this.summaryService.calculateSummary();
+  }
+}
